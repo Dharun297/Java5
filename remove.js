f8cdd77aa154d56e76f7fdaab2e2f23da51a90ca
Dharun297 <dharunpnb@gmail.com>
2023-08-06 20:46:54 +0530

    done

---
+++ remove.js
@@ -0,0 +1,6 @@
+// Remove the first and last elements from an array of strings. (use methods)
+let fruits = ["Apple", "Banana", "Orange", "Grapes"];
+fruits.pop();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
